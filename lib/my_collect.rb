41c4af1ebd5b9fe2903db31a8fc2d197edc4ba92
Nicole Sims <nssims105@gmail.com>
2020-03-15 01:06:51 +0000

    Done.

--- lib/my_collect.rb
+++ lib/my_collect.rb
@@ -1,2 +1,17 @@
+def my_collect(collection)
+  i = 0 
+  collect_array = []
+  
+  while i < collection.length 
+    collect_array << yield(collection[i])
+    i += 1
+  end
+  collect_array
+end 
 
+array = []
+
+my_collect(array) do |name|
+  name.split(" ").first 
+end
 
